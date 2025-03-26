b0b452dbb225e44d12acc7948a3d18e039de5de3
Rudra Dave <rudra.dave@txtech.co>
2025-03-26 10:02:03 +0530

    added portfolio files

---
+++ assets/js/custom.js
@@ -0,0 +1,202 @@
+// Sticky Header
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 90) {
+            $('.header-section').addClass("sticky-top");
+    }
+    else {
+        $('.header-section').removeClass("sticky-top");
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ---------------------------------------------------toggler switcher----------------------------------------------------
+
+const styleswitchertoggle = document.querySelector(".style-switcher-toggler");
+styleswitchertoggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+
+
+
+
+// ---------------------------------------------------hide on slide-------------------------------------------------------
+
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+
+
+// ---------------------------------------------------theme color---------------------------------------------------------
+
+const alternatestyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+    alternatestyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        }
+        else {
+            style.setAttribute("disabled", "true");
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+const cursor = document.querySelector("#cursor");
+const cursorBorder = document.querySelector("#cursor-border");
+const cursorPos = { x: 0, y: 0 };
+const cursorBorderPos = { x: 0, y: 0 };
+
+document.addEventListener("mousemove", (e) => {
+  cursorPos.x = e.clientX;
+  cursorPos.y = e.clientY;
+
+  cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
+
+requestAnimationFrame(function loop() {
+  const easting = 8;
+  cursorBorderPos.x += (cursorPos.x - cursorBorderPos.x) / easting;
+  cursorBorderPos.y += (cursorPos.y - cursorBorderPos.y) / easting;
+
+  cursorBorder.style.transform = `translate(${cursorBorderPos.x}px, ${cursorBorderPos.y}px)`;
+  requestAnimationFrame(loop);
+});
+
+document.querySelectorAll("[data-cursor]").forEach((item) => {
+  item.addEventListener("mouseover", (e) => {
+    if (item.dataset.cursor === "pointer") {
+      cursorBorder.style.backgroundColor = "rgba(255, 255, 255, .6)";
+      cursorBorder.style.setProperty("--size", "30px");
+    }
+    if (item.dataset.cursor === "pointer2") {
+      cursorBorder.style.backgroundColor = "white";
+      cursorBorder.style.mixBlendMode = "difference";
+      cursorBorder.style.setProperty("--size", "80px");
+    }
+  });
+  item.addEventListener("mouseout", (e) => {
+    cursorBorder.style.backgroundColor = "unset";
+    cursorBorder.style.mixBlendMode = "unset";
+    cursorBorder.style.setProperty("--size", "50px");
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var words = document.getElementsByClassName('word');
+var wordArray = [];
+var currentWord = 0;
+
+words[currentWord].style.opacity = 1;
+for (var i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+
+function changeWord() {
+  var cw = wordArray[currentWord];
+  var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+  for (var i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i);
+  }
+  
+  for (var i = 0; i < nw.length; i++) {
+    nw[i].className = 'letter behind';
+    nw[0].parentElement.style.opacity = 1;
+    animateLetterIn(nw, i);
+  }
+  
+  currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+}
+
+function animateLetterOut(cw, i) {
+  setTimeout(function() {
+		cw[i].className = 'letter out';
+  }, i*80);
+}
+
+function animateLetterIn(nw, i) {
+  setTimeout(function() {
+		nw[i].className = 'letter in';
+  }, 340+(i*80));
+}
+
+function splitLetters(word) {
+  var content = word.innerHTML;
+  word.innerHTML = '';
+  var letters = [];
+  for (var i = 0; i < content.length; i++) {
+    var letter = document.createElement('span');
+    letter.className = 'letter';
+    letter.innerHTML = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  
+  wordArray.push(letters);
+}
+
+changeWord();
+setInterval(changeWord, 4000);
+
+
+
+
+
+
+// ---------------------------------------------------light-dark theme----------------------------------------------------
+const daynight = document.querySelector(".day-night");
+daynight.addEventListener("click", () => {
+    daynight.querySelector("i").classList.toggle("fa-sun")
+    daynight.querySelector("i").classList.toggle("fa-moon")
+    document.body.classList.toggle("darkTheme");
+})
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("darkTheme")) {
+        daynight.querySelector("i").classList.add("fa-sun")
+    }
+    else {
+
+        daynight.querySelector("i").classList.add("fa-moon")
+    }
+})
